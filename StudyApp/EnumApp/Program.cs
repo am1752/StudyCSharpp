486262ce13771858f83b138437798c1b3c3882b2
CHAE <am1752@naver.com>
2020-07-06 09:49:34 +0900

    3

---
+++ StudyApp/EnumApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace EnumApp
+{
+    class Program
+    {
+        enum DialogResult
+        {
+            YES = 20,
+            NO = 30,
+            CANCEL = 50,
+            CONFIRM = 60,
+            OK = 70
+        }
+
+        static void Main(string[] args)
+        {
+            /* WriteLine(DialogResult.OK);
+             WriteLine((int)DialogResult.OK);
+             WriteLine(DialogResult.NO); */
+
+            DialogResult result = DialogResult.YES;
+
+            if (result == DialogResult.YES) WriteLine("YES를 선택했습니다!!");
+        }
+    }
+}
